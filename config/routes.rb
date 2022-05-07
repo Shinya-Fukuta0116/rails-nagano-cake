8bcabe1c4e86f8389320d1c4f0cf5c4f3d45318b
EC2 Default User <ec2-user@ip-172-31-95-84.ec2.internal>
2022-05-07 21:52:15 +0000

    edit routes public n admin instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #devise for customer
+  #URL/customers/sign_in..
+
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  #devise for administrator
+  #URL/admin?admin/sign_in..
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+
+
 end
