33b4a1ec6149a091844535f6a9fffb4372db3404
EC2 Default User <ec2-user@ip-172-31-95-84.ec2.internal>
2022-05-19 13:01:01 +0000

    add collection_tag to Item/index, edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new
@@ -11,8 +11,8 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @genre = @item.genre.id
     # byebug
-
     if @item.save
       redirect_to admin_item_path(@item)
     else
@@ -29,6 +29,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path
+    else
+      render "edit"
+    end
+
   end
 
   private
